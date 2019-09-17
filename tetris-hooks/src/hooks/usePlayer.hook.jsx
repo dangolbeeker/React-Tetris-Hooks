116ee77e6996a412f71b6076889242d436fd3c05
dangolbeeker <scottb305@outlook.com>
2019-09-17 00:55:35 -0400

    some methods two usePlayer hook

--- tetris-hooks/src/hooks/usePlayer.hook.jsx
+++ tetris-hooks/src/hooks/usePlayer.hook.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 import { randomTtr } from '../helpers/tetrominos'
+import { pgwidth } from '../helpers/playgroundHelper';
 
 export const usePlayer = () => {
     const [player, setPlayer] = useState({
         pos: { x: 0, y: 0 },
-        tetromino: randomTtr().state,
+        tetromino: randomTtr.shape,
         collided: false
     });
-    return [player];
-} 
+
+    const updatePlayerPos = ({ x, y, collided }) => {
+        setPlayer(prev => ({
+            pos: { x: (player.pos.x + x), y: (player.pos.y + y) },
+            collided: collided
+        }))
+    }
+
+    const resetPlayer = useCallback(
+        () => {
+            setPlayer({
+                pos: { x: pgwidth / 2, y: 0 },
+                tetromino: randomTtr.shape,
+                collided: false
+            })
+        }, [])
+    return [player, updatePlayerPos, resetPlayer];
+}
